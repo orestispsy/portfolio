08a2fea21d6fc81305a7948732eae2f04a2bb4fd
Orestis Psycharis <opsychar@gmail.com>
2021-08-09 12:04:14 +0200

    heroku setup

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,6 @@ module.exports = {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
                 },
