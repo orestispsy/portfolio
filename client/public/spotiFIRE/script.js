b357ac08242c4cc23cffbc457f12735eefb11cdb
Orestis Psycharis <opsychar@gmail.com>
2021-08-10 00:29:16 +0200

    portfolio first layout

---
+++ client/public/spotiFIRE/script.js
@@ -0,0 +1,116 @@
+(function () {
+    var nextUrl;
+    var resultsHTML;
+    var userInput;
+    var artistOrAlbum;
+
+    $(".submit-button").on("click", function () {
+        userInput = $("input").val();
+        artistOrAlbum = $("select").val();
+        requestAjax("https://spicedify.herokuapp.com/spotify");
+    });
+
+    $("#next20").on("click", function () {
+        requestAjax(nextUrl, true);
+    });
+
+    function generateHTML(apiItems) {
+        resultsHTML = "";
+        for (var i = 0; i < apiItems.length; i++) {
+            var defaultImage = "fire&water.jpg";
+            if (apiItems[i].images.length > 0) {
+                defaultImage = apiItems[i].images[0].url;
+            }
+            resultsHTML +=
+                "<div>" +
+                "<a href=" +
+                apiItems[i].external_urls.spotify +
+                ">" +
+                apiItems[i].name +
+                "</a>" +
+                "</div>" +
+                "<a href=" +
+                apiItems[i].external_urls.spotify +
+                ">" +
+                '<img src="' +
+                defaultImage +
+                '" />' +
+                "</a>";
+        }
+        return resultsHTML;
+    }
+
+    function requestAjax(urlRequest, more20) {
+        $.ajax({
+            method: "GET",
+            url: urlRequest,
+            data: {
+                query: userInput,
+                type: artistOrAlbum,
+            },
+            success: function (apiResponse) {
+                apiResponse = apiResponse.artists || apiResponse.albums;
+                console.log("apiResponse: ", apiResponse);
+
+                if (apiResponse.items.length == 0) {
+                    $(".searchINFO").css("visibility", "visible");
+                    $(".searchINFO").html(
+                        "<div>" + "Sorry no results" + "</div>"
+                    );
+                } else {
+                    $(".searchINFO").css("visibility", "visible");
+                    $(".searchINFO").html(
+                        "<div>" +
+                            "WE've got " +
+                            apiResponse.total +
+                            " results for you !" +
+                            "</div>"
+                    );
+                }
+
+                if (more20) {
+                    $(".results-container").append(
+                        generateHTML(apiResponse.items)
+                    );
+                } else {
+                    $(".results-container").html(
+                        generateHTML(apiResponse.items)
+                    );
+                }
+
+                nextUrl =
+                    apiResponse.next &&
+                    apiResponse.next.replace(
+                        "api.spotify.com/v1/search",
+                        "spicedify.herokuapp.com/spotify"
+                    );
+
+                if (location.search.indexOf("?scroll=infinite") > -1) {
+                    if (apiResponse.next === null) {
+                        return;
+                    }
+                    function check4Infinity() {
+                        // console.log($(window).scrollTop());
+                        // console.log($(window).height());
+                        // console.log($(document).height());
+                        if (
+                            $(window).scrollTop() + $(window).height() >=
+                            $(document).height() - 300
+                        ) {
+                            requestAjax(nextUrl, true);
+                        } else {
+                            setTimeout(check4Infinity, 1000);
+                        }
+                    }
+                    check4Infinity();
+                } else {
+                    if (apiResponse.next !== null) {
+                        $("#next20").css("visibility", "visible");
+                    } else {
+                        $("#next20").css("visibility", "hidden");
+                    }
+                }
+            },
+        });
+    }
+})();
--- client/src/start.js
+++ client/src/start.js
@@ -6,14 +6,87 @@ class App extends React.Component {
         return (
             <div className="appContainer">
                 <div className="appBox">
-                    <div>Welcome </div>
-                    <div className="selectedProject">
-                        <div className="name">Yolo</div>
-                        <img className="imgScr" src="astro.png"></img>
+                    <div className="introMenu">
+                        <div className="introText">Orestis Psycharis</div>
+                        <div>
+                            <a href="#">
+                                <img
+                                    src="./linkedIn.png"
+                                    className="linkedIn"
+                                ></img>
+                            </a>
+                            <a
+                                href="https://github.com/orestispsy/"
+                                target="_blank"
+                            >
+                                <img
+                                    src="./gitHub.png"
+                                    className="gitHub"
+                                ></img>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="introTitles">
+                        Javascript Developer, focused on React.js | Node.js |
+                        Express.js | Redux | PostgreSQL | Socket.io
+                    </div>
+                    <div className="intro">LATEST PROJECTS</div>
+                </div>
+                <div className="projectBoxBack">
+                    <div className="projectBox">
+                        <a
+                            href="https://thousandgigs.herokuapp.com"
+                            className="project"
+                            target="_blank"
+                        >
+                            <div className="name">The Gig Guide</div>
+                            <img
+                                className="imgScr"
+                                src="preview1kgg.jpg"
+                                id="three"
+                            ></img>
+                        </a>
+                        <a
+                            href="/spotiFIRE/index.html"
+                            className="project"
+                            target="_blank"
+                        >
+                            <div className="name">SpotiFIRE!</div>
+                            <img
+                                className="imgScr"
+                                src="./spotiFIRE/preview.jpg"
+                                id="one"
+                            ></img>
+                        </a>
+
+                        <a
+                            href="/connect4/index.html"
+                            className="project"
+                            target="_blank"
+                        >
+                            <div className="name">Connect4</div>
+                            <img
+                                className="imgScr"
+                                src="/connect4/preview.jpg"
+                                id="two"
+                            ></img>
+                        </a>
+                        <a
+                            href="https://warm-spire-33241.herokuapp.com/"
+                            className="project"
+                            target="_blank"
+                        >
+                            <div className="name">Petition</div>
+                            <img
+                                className="imgScr"
+                                src="previewPetition.jpg"
+                                id="four"
+                            ></img>
+                        </a>
                     </div>
                 </div>
             </div>
         );
     }
 }
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.querySelector("main"));
