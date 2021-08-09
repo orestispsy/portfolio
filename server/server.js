c320cd5c5c836faaffcbc2b186497bc2ad4d0187
Orestis Psycharis <opsychar@gmail.com>
2021-08-09 13:28:10 +0200

    first commit

--- server/server.js
+++ server/server.js
@@ -3,18 +3,13 @@ var express = require("express");
 
 var app = express();
 
-   app.use(express.static(path.join(__dirname, "..", "client", "public")));
+app.use(express.static(path.join(__dirname, "..", "client", "public")));
 app.set("port", process.env.PORT || 3001);
 
 app.get("*", function (req, res) {
-
-        res.sendFile(path.join(__dirname, "..", "client", "index.html"));
-    
+    res.sendFile(path.join(__dirname, "..", "client", "index.html"));
 });
 
 var server = app.listen(app.get("port"), function () {
-    console.log(
-        "🟢 Portfolio Project Listening Port:",
-        server.address().port
-    );
+    console.log("🟢 Portfolio Project Listening Port:", server.address().port);
 });
