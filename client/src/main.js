190e4208174eb5e1089608bc30c14d12f97591e5
Orestis Psycharis <opsychar@gmail.com>
2021-09-11 13:08:27 +0200

    fix responsive design

--- client/src/main.js
+++ client/src/main.js
@@ -103,7 +103,7 @@ export default function Main({
                                         });
                                     }}
                                 >
-                                    show less
+                                    hide
                                 </span>
                             )}
                         </div>{" "}
